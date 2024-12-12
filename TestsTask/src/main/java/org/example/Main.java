b5c7adb481354e1a13b4fd7e75e269b9d9577000
ManyL152-test <malina.tester.rus@gmail.com>
2024-12-13 01:41:05 +0300

    task 2

--- TestsTask/src/main/java/org/example/Main.java
+++ TestsTask/src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello,  World!");
+        int a = 1;
+        boolean b = true;
+        byte c = 3;
+        short d = 4;
+        long e = 5;
+        double f = 6;
+        float g = 7;
+        char o = 6553;
+        System.out.println("a = " + a + " b = " + b + " c = "
+                + c + " d = " + d + " e = " + e + " f = " + f + " g = " + g + " o = " + o);
     }
 }
