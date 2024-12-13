a10b7b8c0e0743ea4f2fa151fd4b6716f3bbf9b7
ManyL152-test <malina.tester.rus@gmail.com>
2024-12-13 04:31:09 +0300

    test 9

--- TestsTask/src/main/java/org/example/Main.java
+++ TestsTask/src/main/java/org/example/Main.java
@@ -1,16 +1,20 @@
 package org.example;
-import java.util.*;
+
+class Animal {
+    public void speak() {
+        System.out.println("Animal говорит...");
+    }
+}
+
+class Dog extends Animal {
+    public void speak() {
+        System.out.println("Собака лает... Гав-гав!");
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-
-        List<String> list = new ArrayList<>();
-        list.add("Java");
-        list.add("Test");
-        list.add("Сергей");
-        list.add("Admin");
-
-        Collections.sort(list);
-        System.out.println("Отсортированный список: " + list);
+        Dog dog = new Dog();
+        dog.speak();
     }
 }
