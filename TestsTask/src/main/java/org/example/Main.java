7a4c0a0dfff0e6ff89075dd3af381f767d4b9019
ManyL152-test <malina.tester.rus@gmail.com>
2024-12-13 04:08:58 +0300

    test 8

--- TestsTask/src/main/java/org/example/Main.java
+++ TestsTask/src/main/java/org/example/Main.java
@@ -1,25 +1,16 @@
 package org.example;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        int[] numbers = new int[5];
+        List<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("Test");
+        list.add("Сергей");
+        list.add("Admin");
 
-        Scanner scanner = new Scanner(System.in);
-        
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println("Введите число " + (i + 1) + ":");
-            numbers[i] = scanner.nextInt();
-        scanner.close();
-
-        int maxValue = numbers[0];
-        for (int num : numbers) {
-            if (num > maxValue) {
-                maxValue = num;
-            }
-        }
-
-        System.out.println("Максимальное значение в массиве: " + maxValue);
+        Collections.sort(list);
+        System.out.println("Отсортированный список: " + list);
     }
 }
