424d03e463c66c8d1e6285274bfbcae66e69e281
ManyL152-test <malina.tester.rus@gmail.com>
2024-12-16 08:16:46 +0300

    test 12

---
+++ TestsTask/src/main/java/org/example/ReadFile.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ReadFile {
+    public static void main(String[] args) throws IOException {
+        // Создаем объект класса File для работы с файлом
+        File file = new File("test 12.txt");
+
+        if (file.exists()) {
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+
+            scanner.close();
+        } else {
+            System.out.println("Файл не найден.");
+        }
+    }
+}
